5213f9f8a158472b5409ec9c356e0d95db46c1aa
IsaacEsk <isaac.pineda@eskayser.com.mx>
2026-04-22 17:45:48 -0600

    cambio conector

--- scripts/config.js
+++ scripts/config.js
@@ -2,8 +2,8 @@
 
 
 // URL del backend (editar aquí para cambiar entre local / prod)
-window.BACKEND_HOST = 'http://localhost:3000';
+//window.BACKEND_HOST = 'http://localhost:3000';
 
 // Para producción, descomenta la línea siguiente:
- //window.BACKEND_HOST = 'https://sincronizapkbackend.onrender.com';
+window.BACKEND_HOST = 'https://sincronizapkbackend.onrender.com';
 
